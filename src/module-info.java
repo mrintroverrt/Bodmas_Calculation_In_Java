441d0ddf50294b6e07e7372bd465e5c65c7c077d
mrintroverrt <prasaanth2k@gmail.com>
2023-03-28 14:31:09 +0530

    Boadmas Example

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Home
+ *
+ */
+module Bodmas_Calculation {
+}
---
+++ src/org/bod/Boadmas.java
@@ -0,0 +1,10 @@
+package org.bod;
+
+public class Boadmas {
+	public static void main(String[] args) {
+		int a = 2, b = 3;
+		double x = (a + Math.pow(b, 2) + Math.pow(a + b, 2))/2;
+		System.out.println(x);
+	}
+
+}
